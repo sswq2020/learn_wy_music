327ab7534d15574bd3843a5b24eebcc4a35fa7cd
sswq2020 <a309108243@qq.com>
2018-04-26 10:04:38 +0800

    歌手res的数据是杂乱，需要自己序列化，用到for...in,obj[key],sort,charCodeAt作为数组方法不错的参考练习

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -3,10 +3,75 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer.js'
 
-  }</script>
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+export default {
+    data () {
+        return {
+            singers: []
+        }
+    },
+    created() {
+        this._getSingerList()
+    },
+    methods: {
+        _getSingerList() {
+            getSingerList().then((res) => {
+                if (res.code === ERR_OK) {
+                    this.singers = res.data.list
+                    console.log(this._normalizeSinger(this.singers))
+                }
+            })
+        },
+        _normalizeSinger(list) {
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    items: []
+                }
+            }
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LEN) {
+                    map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+                }
+                const key = item.Findex
+                if (!map[key]) {
+                    map[key] = {
+                        title: key,
+                        items: []
+                    }
+                }
+                map[key].items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+            })
+            let hot = []
+            let ret = []
+            for (let key in map) {
+                let val = map[key]
+                if (val.title.match(/a-zA-Z/)) {
+                    ret.push(val)
+                } else if (val.title === HOT_NAME) {
+                    hot.push(val)
+                }
+            }
+            ret.sort((a, b) => {
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+            })
+            return hot.concat(ret)
+        }
+    }
+}
+</script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+    .singer
+      position:fixed
+      top: 88px
+      bottom: 0 
+      width: 100%
 </style>
