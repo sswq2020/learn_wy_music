4dd9cce679b44aee9523a1c1425203b486e6056b
sswq2020 <a309108243@qq.com>
2018-09-07 16:25:28 +0800

    music-list增加scroll组件实现滑动,同时监听滚动值

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="music-list">
-    <div class="back">
-      <i class="icon-back"></i>
-    </div>
+    <div class="back"><i class="icon-back"></i></div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle">
-      <div class="filter">
-
-      </div>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="filter" ref="filter"></div>
     </div>
-    <div class="bg-layer"></div>
-    <div class="list"></div>
+    <div class="bg-layer" ref="layer"></div>
+    <scroll
+      :data="songs"
+      @scroll="scroll"
+      :probe-type="probeType"
+      :listen-scroll="listenScroll"
+      class="list"
+      ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs "></song-list>
+      </div>
+    </scroll>
+
   </div>
 </template>
 
 <script  type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll.vue'
+import SongList from 'base/song-list/song-list'
+const RESERVED_HEIGHT = 40
 export default {
     props: {
         bgImage: {
@@ -30,9 +40,64 @@ export default {
             default: ''
         }
     },
+    data() {
+        return {
+            scrollY: 0
+        }
+    },
+    methods: {
+        scroll(pos) {
+            this.scrollY = pos.y
+        }
+    },
     computed: {
         bgStyle() {
             return `background-image:url(${this.bgImage}) `
+        }
+    },
+    components: {
+        Scroll,
+        SongList
+    },
+    created() {
+        this.probeType = 3
+        this.listenScroll = true
+    },
+    mounted() {
+        this.imageHeight = this.$refs.bgImage.clientHeight
+        this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+        this.$refs.list.$el.style.top = `${this.imageHeight}px`
+    },
+    watch: {
+        scrollY(newY) {
+            let translateY = Math.max(this.minTranslateY, newY)
+            let zIndex = 0 // 图片层级
+            let scale = 1 // 图片缩放
+            let blur = 0 // 高斯模糊,IOS设备
+            this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+            this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+            const percent = Math.abs(newY / this.minTranslateY)
+            if (newY > 0) {
+                scale = 1 + percent
+                zIndex = 10
+            } else {
+                blur = Math.min(20 * percent, 20)
+            }
+
+            if (newY < this.minTranslateY) {
+                zIndex = 10
+                this.$refs.bgImage.style.paddingTop = 0
+                this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+            } else {
+                this.$refs.bgImage.style.paddingTop = '70%'
+                this.$refs.bgImage.style.height = 0
+            }
+            this.$refs.bgImage.style.zIndex = zIndex
+            this.$refs.bgImage.style['transform'] = `scale(${scale})`
+            this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+
+            this.$refs.filter.style['backdrop-filter'] = `blur(${blur})`
+            this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur})`
         }
     }
 }
@@ -63,6 +128,7 @@ export default {
     position:absolute
     top:0
     left:10%
+    z-index:40
     width:80%
     text-overflow:ellipsis
     overflow:hidden
@@ -71,7 +137,7 @@ export default {
     line-height:40px
     font-size:$font-size-large
     color:$color-text
-    z-index:40
+
   .bg-image
     position:relative
     width:100%
@@ -79,12 +145,13 @@ export default {
     padding-top:70%
     background-size:cover
     -webkit-transform-origin:top
-    z-index:1
+    transform-origin:top
     .filter
       position:absolute
       top:0
       left:0
       width:100%
+      height:100%
       background:rgba(7,17,27,.4)
   .bg-layer
     position:relative
@@ -95,7 +162,10 @@ export default {
     top:0
     bottom:0
     width:100%
+    // overflow:hidden
     background:$color-background
+    .song-list-wrapper
+        padding:20px 30px
 </style>
 
 
