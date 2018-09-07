1f021aaaae5b210306470acce79b74d0ebd1a2de
sswq2020 <a309108243@qq.com>
2018-09-07 16:27:01 +0800

    song-list组件基本样式搭建

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li class="item" v-for="song in songs">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script  type="text/ecmascript-6">
+export default {
+    props: {
+        songs: {
+            type: Array,
+            default: []
+        }
+    },
+    methods: {
+        getDesc(song) {
+            return `${song.singer}·${song.album}`
+        }
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+.song-list
+  .item
+    display:flex
+    align-items:center
+    box-sizing:border-box
+    height:64px
+    font-size:$font-size-medium
+    .content
+      flex:1
+      line-height:20px
+      overflow:hidden
+      .name
+        text-overflow:ellipsis
+        overflow:hidden
+        white-space:nowrap
+        color:$color-text
+      .desc
+        text-overflow:ellipsis
+        overflow:hidden
+        white-space:nowrap
+        color:$color-text-d
+        margin-top:4px
+    .rank
+      flex: 0 0 25px
+      width:25px
+      margin-right:30px
+      text-align:center
+
+
+</style>
+
+
+
