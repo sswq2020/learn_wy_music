e52749cae22228eaff1715a7b6d9cf6485a921b2
sswq2020 <a309108243@qq.com>
2018-09-14 17:22:02 +0800

    将播放器组件放入App.vue全局组件中

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import mHeader from 'components/m-header/m-header'
   import Tab from 'components/tab/tab'
+  import player from 'components/player/player'
   export default {
       name: 'app',
       components: {
           mHeader,
-          Tab
+          Tab,
+          player
       }
   }</script>
 
