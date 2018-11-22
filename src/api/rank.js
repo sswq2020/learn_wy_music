8085d94c795cbc09a22a544992ce33e219d7b291
sswq2020 <a309108243@qq.com>
2018-11-22 10:19:53 +0800

    增加排行磅接口

---
+++ src/api/rank.js
@@ -0,0 +1,18 @@
+import { commonParams } from 'api/config'
+import axios from 'axios'
+export function getDiscList() {
+    const url = '/api/getToplists'
+
+    const data = Object.assign({}, commonParams, {
+        platform: 'h5',
+        uin: 0,
+        notice: 0,
+        sortId: 5,
+        needNewCode: 1,
+        categoryId: 10000000,
+        format: 'json'
+    })
+    return axios.get(url, { params: data }).then((res) => {
+        return Promise.resolve(res.data)
+    })
+}
