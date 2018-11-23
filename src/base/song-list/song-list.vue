9ac3c22c641f71ed2e2b7e97aa36dab88ff711d3
sswq2020 <a309108243@qq.com>
2018-11-23 16:30:12 +0800

    扩展song-list适应top-list组件

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -2,6 +2,9 @@
   <div class="song-list">
     <ul>
       <li class="item" @click="selectItem(song,index)" v-for="(song,index) in songs">
+        <div class="rankIcon" v-show="rank">
+          <span :class="getIconClass(index)">{{getIconText(index)}}</span>
+        </div>
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -17,6 +20,10 @@ export default {
         songs: {
             type: Array,
             default: () => []
+        },
+        rank: {
+            type: Boolean,
+            default: false
         }
     },
     methods: {
@@ -25,43 +32,67 @@ export default {
         },
         selectItem(item, index) {
             this.$emit('select', item, index)
+        },
+        getIconClass(index) {
+            if (index < 3) {
+                return `icon icon${index}`
+            } else {
+                return 'icon'
+            }
+        },
+        getIconText(index) {
+            if (index < 3) {
+                return ''
+            } else {
+                return `${index + 1}`
+            }
         }
     }
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-@import "~common/stylus/mixin"
-.song-list
+<style lang="stylus" scoped>
+ @import "~common/stylus/variable"
+ @import "~common/stylus/mixin"
+
+ .song-list
   .item
-    display:flex
-    align-items:center
-    box-sizing:border-box
-    height:64px
-    font-size:$font-size-medium
+    display flex
+    align-items center
+    box-sizing border-box
+    height 64px
+    font-size $font-size-medium
+    .rankIcon
+      flex 0 0 25px
+      width 25px
+      margin-right 30px
+      text-align center
+      .icon
+        display inline-block
+        width 25px
+        height 24px
+        background-size 25px 24px
+        &.icon0
+           bg-image('first')
+        &.icon1
+           bg-image('second')
+        &.icon2
+           bg-image('third')
     .content
-      flex:1
-      line-height:20px
-      overflow:hidden
+      flex 1
+      line-height 20px
+      overflow hidden
       .name
-        text-overflow:ellipsis
-        overflow:hidden
-        white-space:nowrap
-        color:$color-text
+        text-overflow ellipsis
+        overflow hidden
+        white-space nowrap
+        color $color-text
       .desc
-        text-overflow:ellipsis
-        overflow:hidden
-        white-space:nowrap
-        color:$color-text-d
-        margin-top:4px
-    .rank
-      flex: 0 0 25px
-      width:25px
-      margin-right:30px
-      text-align:center
-
-
+        text-overflow ellipsis
+        overflow hidden
+        white-space nowrap
+        color $color-text-d
+        margin-top 4px
 </style>
 
 
