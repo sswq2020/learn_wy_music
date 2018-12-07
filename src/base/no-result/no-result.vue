f625a248fa63d4129580cc60b1dd7f6d959aa82b
sswq2020 <a309108243@qq.com>
2018-12-07 15:36:49 +0800

    增加no-result组件

---
+++ src/base/no-result/no-result.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="no-result">
+    <div class="no-result-icon"></div>
+    <p class="no-result-text">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+<style lang="stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .no-result
+    text-align center
+    .no-result-icon
+      width 86px
+      height 90px
+      margin 0 auto
+      background-size 86px 90px
+      bg-image('no-result')
+    .no-result-text
+      margin-top 30px
+      font-size  $font-size-medium
+      color $color-text-d
+</style>
+
