64163b5f4e925feacc31c8ed10460911403d402c
sswq2020 <a309108243@qq.com>
2018-12-28 17:25:46 +0800

    增加vuex的searchHistroy的state及其配套

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -27,6 +27,9 @@ const mutations = {
     },
     [types.SET_CURRENT_INDEX](state, index) {
         state.currentIndex = index
+    },
+    [types.SET_SEARCH_HISTORY](state, history) {
+        state.searchHistroy = history
     }
 }
 
--- src/store/state.js
+++ src/store/state.js
@@ -8,7 +8,8 @@ const state = {
     mode: playMode.sequence, // 播放模式
     currentIndex: -1, // 当前播放哪首歌,
     disc: {}, // 歌单
-    toplist: {} // 排行耪
+    toplist: {}, // 排行耪
+    searchHistroy: [] // 搜素过的历史
 }
 
 export default state
