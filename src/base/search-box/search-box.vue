654cda55b20db64902319fef4675aa1c6441781d
sswq2020 <a309108243@qq.com>
2018-12-28 17:07:56 +0800

    1.search-box.vue add ref;2.对外提供blur事件

--- src/base/search-box/search-box.vue
+++ src/base/search-box/search-box.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-box">
     <i class="icon-search"></i>
-    <input type="text" v-model="query" :placeholder="placeholder" class="box">
+    <input ref="query" type="text" v-model="query" :placeholder="placeholder" class="box">
     <i @click="clear" v-show="query" class="icon-dismiss"></i>
   </div>
 </template>
@@ -26,6 +26,9 @@ export default {
         },
         setQuery(query) {
             this.query = query
+        },
+        blur() {
+            this.$refs.query.blur()
         }
     },
     created() {
