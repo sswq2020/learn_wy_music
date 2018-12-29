b348c8a1195a0855b6869322e9d693a3e2363dee
sswq2020 <a309108243@qq.com>
2018-12-29 15:14:41 +0800

    增加缓存保存到本地的cache库

---
+++ src/common/js/cache.js
@@ -0,0 +1,28 @@
+import storage from 'good-storage'
+const SEARCH_KEY = '__search__'
+
+const SEARCH_MAX_LENGTH = 15
+
+export function saveSearch(query) {
+    debugger
+    let searches = storage.get(SEARCH_KEY, [])
+    insertArray(searches, query, SEARCH_MAX_LENGTH, (item) => {
+        return item === query
+    })
+    storage.set(SEARCH_KEY, searches)
+    return searches
+}
+
+function insertArray(arr, val, maxLen, compare) {
+    const index = arr.findIndex(compare)
+    if (index === 0) {
+        return
+    }
+    if (index > 0) {
+        arr.splice(index, 1)
+    }
+    arr.unshift(val)
+    if (maxLen && arr.length > maxLen) {
+        arr.pop()
+    }
+}
