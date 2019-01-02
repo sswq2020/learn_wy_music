218cd3472e1edca32dbfa10382fdbc86408ba808
sswq2020 <a309108243@qq.com>
2019-01-02 14:46:17 +0800

    修复搜索历史初始化的bug,是个函数语句而不是函数定义

--- src/store/state.js
+++ src/store/state.js
@@ -10,7 +10,7 @@ const state = {
     currentIndex: -1, // 当前播放哪首歌,
     disc: {}, // 歌单
     toplist: {}, // 排行耪
-    searchHistroy: loadSearch // 搜素过的历史,从本地localStorage读取
+    searchHistroy: loadSearch() // 搜素过的历史,从本地localStorage读取
 }
 
 export default state
