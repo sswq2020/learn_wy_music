97c5eb72adcba3f3f23c232231f1f17546e77ed1
sswq2020 <a309108243@qq.com>
2019-01-02 14:47:28 +0800

    删除数组的方法

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -16,8 +16,8 @@ export function loadSearch() {
     return storage.get(SEARCH_KEY, [])
 }
 
-function insertArray(arr, val, maxLen, compare) {
-    const index = arr.findIndex(compare)
+function insertArray(arr, val, maxLen, cb = () => {}) {
+    const index = arr.findIndex(cb)
     if (index === 0) {
         return
     }
@@ -28,4 +28,25 @@ function insertArray(arr, val, maxLen, compare) {
     if (maxLen && arr.length > maxLen) {
         arr.pop()
     }
+}
+
+function deleteArray(arr, cb = () => {}) {
+    const index = arr.findIndex(cb)
+    if (index > -1) {
+        arr.splice(index, 1)
+    }
+}
+export function deleteSearchHistoryItem(history) {
+    debugger
+    let searches = storage.get(SEARCH_KEY, [])
+    deleteArray(searches, (item) => {
+        return history === item
+    })
+    storage.set(SEARCH_KEY, searches)
+    return searches
+}
+
+export function deleteSearchHistoryAll(history) {
+    storage.set(SEARCH_KEY, [])
+    return []
 }
