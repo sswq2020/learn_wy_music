c15f46688ea12bdbf9091df225717baffaf85f8d
sswq2020 <a309108243@qq.com>
2019-04-03 14:33:36 +0800

    新增top-tip组件

---
+++ src/base/top-tip/top-tip.vue
@@ -0,0 +1,28 @@
+<template >
+  <transiton name="drop">
+    <div class="top-tip">
+      <slot></slot>
+    </div>
+  </transiton>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+ @import "~common/stylus/variable"
+ @import "~common/stylus/mixin"
+ .top-tip
+   position fixed
+   top 0
+   width 100%
+   z-index 500
+   background $color-dialog-background
+   &.drop-enter-active,&.drop-leave-active
+     transition all .3s
+   &.drop-enter,&.drop-leave-to
+     transform translate3d(0,-100%,0)
+</style>
